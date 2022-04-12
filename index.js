36ea4a577445b9c5ed7757b1824c2ebdeb46198b
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2022-04-11 20:11:51 -0500

    Adding config dependencies for express

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 
 const express = require('express');
+const expressConfig = require('./config/expressConf');
 
 // Express server
 const app = express();
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
+  expressConfig.config(app);
+
   // eslint-disable-next-line no-console
   console.log(`App is running on port ${port}`);
 });
