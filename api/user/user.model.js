eb481da4f693c02d4ca9cfd21e22a12ccdec0e9d
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2022-04-16 12:45:51 -0500

    Creating user model

---
+++ api/user/user.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_WORK_FACTOR = 10;
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      index: { unique: true },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Favorite',
+    },
+    token: { type: String },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+UserSchema.pre('save', function save(next) {
+  const user = this;
+  if (!user.isModified('password')) next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, (saltError, salt) => {
+    if (saltError) {
+      next(saltError);
+    }
+    bcrypt.hash(user.password, salt, (hashError, hash) => {
+      if (hashError) {
+        next(hashError);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+module.exports = mongoose.model('User', UserSchema);
